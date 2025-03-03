7bd509ca3824b0c53396e43847182b192d23208e
Your Name <danagabay@msn.com>
2025-03-03 13:51:22 -0500

    models added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from main_app.models import Project, About
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(About)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    wireframe = models.ImageField()
+    trello = models.URLField(blank=True, null=True)
+    case_study = models.TextField()
+    url = models.URLField(blank=True)
+    github_url = models.URLField(blank=True)
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class About(models.Model):
+    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    bio = models.TextField()
+    profile_image = models.ImageField(blank=True, null=True)
+    resume = models.FileField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
