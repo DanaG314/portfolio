1847140cfe86c4bd73f8b4c252203706755a7828
Your Name <danagabay@msn.com>
2025-03-04 15:45:38 -0500

    about me page styling added

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from main_app.models import About, Project
 
 
 # Create your views here.
@@ -7,6 +8,11 @@ from django.http import HttpResponse
 
 class Home(TemplateView):
     template_name = 'main_app/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['about'] = About.objects.first()
+        return context
 
 def about(request):
     return render(request, 'main_app/about.html')
