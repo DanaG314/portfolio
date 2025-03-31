e4a60c80a1380e0fda01b3f1d03bcbef0c459c5f
Your Name <danagabay@msn.com>
2025-03-31 19:21:52 -0400

    context added to home class, projects removed

--- main_app/views.py
+++ main_app/views.py
@@ -12,18 +12,25 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['about'] = About.objects.first()
+
+        projects = Project.objects.all()
+        context['simon']= projects.filter(title='Simon!').first()
+        context['binge_buddy']= projects.filter(title='Binge Buddy').first()
+        context['legodex']= projects.filter(title='LegoDex').first()
+        context['shelf_space']= projects.filter(title='Shelf Space').first()
+        
         return context
 
 def about(request):
     return render(request, 'main_app/about.html')
 
 
-def projects_page(request):
-    projects = Project.objects.all()
-    context = {
-        'simon': projects.filter(title='Simon!').first(),
-        'binge_buddy': projects.filter(title='Binge Buddy').first(),
-        'legodex': projects.filter(title='LegoDex').first(),
-        'shelf_space': projects.filter(title='Shelf Space').first(),
-    }
-    return render(request, 'main_app/projects.html', context)
+# def projects_page(request):
+#     projects = Project.objects.all()
+#     context = {
+#         'simon': projects.filter(title='Simon!').first(),
+#         'binge_buddy': projects.filter(title='Binge Buddy').first(),
+#         'legodex': projects.filter(title='LegoDex').first(),
+#         'shelf_space': projects.filter(title='Shelf Space').first(),
+#     }
+#     return render(request, 'main_app/projects.html', context)
